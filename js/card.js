3a24797aa248df342ff379496e03977179f134d2
SantiG95 <santiguerra86@gmail.com>
2021-11-01 17:20:31 -0300

    Las tarjetas tienen para comprar distintas unidades

--- js/card.js
+++ js/card.js
@@ -1,9 +1,29 @@
 var listaCartas = document.getElementsByClassName("card")
+var listaStock  = []
+
 
 for(var i = 0; i < listaCartas.length; i++){
-    listaCartas[i].addEventListener("click", miFuncion)
-}
+    //Se imprime en la consola el mensaje "Se compro (nombre del producto)" 
+    //al hacer clic en la carta
+    /*listaCartas[i].addEventListener("click", function(){
+        console.log("Se compro: " + this.getElementsByClassName("card__heading")[0].innerHTML)
+        }
+    )
+*/
+    //Botones para cambiar cantidad
+    var botones = listaCartas[i].getElementsByTagName("input")
+    
+    botones[0].addEventListener("click", function(){
+        if(this.parentElement.getElementsByClassName("product_quantity")[0].innerHTML > 0){
+            this.parentElement.getElementsByClassName("product_quantity")[0].innerHTML = parseInt(this.parentElement.getElementsByClassName("product_quantity")[0].innerHTML) - 1
+        }
+        
+    })
 
-function miFuncion(){
-    console.log("Se compro: " + this.getElementsByClassName("card__heading")[0].innerHTML)
+    botones[1].addEventListener("click", function(){
+        this.parentElement.getElementsByClassName("product_quantity")[0].innerHTML = parseInt(this.parentElement.getElementsByClassName("product_quantity")[0].innerHTML) + 1
+    })
+    
+
+
 }
